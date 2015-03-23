25a5c7ffd982cc08df0866b0c5be0f8ace7c9915
AlexGuMusic <y-n7@live.com>
2015-03-23 19:12:11 +0300

    adding functions

--- js/hangman.js
+++ js/hangman.js
@@ -5,3 +5,29 @@ var canvas = document.getElementById('stage'),
  wordLength,
  badGuesses,
  correctGuesses;
+ 
+ function init() { 
+ var helptext = $('#helptext'),
+ w = screen.availWidth <= 800 ? screen.availWidth : 800; // Hide the loading message and display the control buttons
+ $('#loading').hide();
+ $('#play').css('display', 'inline-block').click(newGame);
+ $('#clear').css('display', 'inline-block').click(resetScore);
+ $('#help').click(function(e) {
+ $('body').append('<div id="mask"></div>');
+ helptext.show().css('margin-left', (w-300)/2 + 'px');
+ });
+$('#close').click(function(e) {
+ $('#mask').remove();
+ helptext.hide();
+ });
+ 
+ }
+ 
+ function newGame() {
+ var placeholders = '',
+ frag = document.createDocumentFragment(),
+ abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M', 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+ }
+ function resetScore() {
+ alert('Score has been reset'); 
+ }
