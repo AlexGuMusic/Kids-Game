d482cdba9e2139d6865573ebacd757307ebfca76
AlexGuMusic <y-n7@live.com>
2015-03-23 19:54:42 +0300

    correcting the script

--- js/hangman.js
+++ js/hangman.js
@@ -1,33 +1,46 @@
 var canvas = document.getElementById('stage'),
- word = document.getElementById('word'),
- letters = document.getElementById('letters'),
- wordToGuess,
- wordLength,
- badGuesses,
- correctGuesses;
- 
- function init() { 
- var helptext = $('#helptext'),
- w = screen.availWidth <= 800 ? screen.availWidth : 800;
- $('#loading').hide();
- $('#play').css('display', 'inline-block').click(newGame);
- $('#clear').css('display', 'inline-block').click(resetScore);
- $('#help').click(function(e) {
- $('body').append('<div id="mask"></div>');
- helptext.show().css('margin-left', (w-300)/2 + 'px');
- });
-$('#close').click(function(e) {
- $('#mask').remove();
- helptext.hide();
- });
- 
- }
- 
- function newGame() {
- var placeholders = '',
- frag = document.createDocumentFragment(),
- abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M', 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
- }
- function resetScore() {
- alert('Score has been reset'); 
- }
+	word = document.getElementById('word'),
+	letters = document.getElementById('letters'),
+	wordToGuess,
+	wordLength,
+	badGuesses,
+	correctGuesses;
+
+function init() {
+	var helptext = $('#helptext'),
+		w = screen.availWidth <= 800 ? screen.availWidth : 800;
+	
+	$('#loading').hide();
+	$('#play').css('display', 'inline-block').click(newGame);
+	$('#clear').css('display', 'inline-block').click(resetScore);
+	$('#help').click(function(e) {
+		$('body').append('<div id="mask"></div>');
+        helptext.show().css('margin-left', (w-300)/2 + 'px');
+    });
+	$('#close').click(function(e) {
+		$('#mask').remove();
+        helptext.hide();
+    });
+	
+	
+}
+
+
+function newGame() {
+	var placeholders = '',
+		frag = document.createDocumentFragment(),
+		abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+	badGuesses = 0;
+	correctGuesses = 0;
+	wordToGuess = getWord();
+	wordLength = wordToGuess.length;
+	
+	for (var i = 0; i < wordLength; i++) {
+		placeholders += '_';
+	}
+	word.innerHTML = placeholders;	
+	letters.innerHTML = '';
+	
+}
+
+
