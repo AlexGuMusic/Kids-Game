395b284f06d9ea085738003f32d7d88a3500c726
AlexGuMusic <y-n7@live.com>
2015-03-28 18:41:26 +0300

    starting the convas drwaing

--- js/hangman.js
+++ js/hangman.js
@@ -64,8 +64,97 @@ function getLetter() {
  }
  
 function checkLetter(letter) {
-	
+	var placeholders = word.innerHTML,
+	    wrongGuess = true;
+			placeholders = placeholders.split('');	
+	for (var i = 0; i < wordLength; i++) {
+		if (wordToGuess.charAt(i) == letter.toLowerCase()) {
+			placeholders[i] = letter;
+			wrongGuess = false;
+			correctGuesses++;
+			if (correctGuesses == wordLength) {
+				drawCanvas();
+			}
+		}
+	}
+	if (wrongGuess) {
+		badGuesses++;
+		drawCanvas();
+	}
+	word.innerHTML = placeholders.join('');
+		
 }
+
+function drawCanvas() {
+	var c = canvas.getContext('2d');
+		canvas.width = canvas.width;
+	c.lineWidth = 10;
+	c.strokeStyle = 'green';
+	c.font = 'bold 24px Optimer, Arial, Helvetica, sans-serif';
+	c.fillStyle = 'red';
+	drawLine(c, [20,190], [180,190]);
+	if (badGuesses > 0) {
+		c.strokeStyle = '#A52A2A';
+		drawLine(c, [30,185], [30,10]);
+		if (badGuesses > 1) {
+			c.lineTo(150,10);
+			c.stroke();
+		}
+		if (badGuesses > 2) {
+			c.strokeStyle = 'black';
+			c.lineWidth = 3;
+			drawLine(c, [145,15], [145,30]);
+			c.beginPath();
+			c.moveTo(160, 45);
+			c.arc(145, 45, 15, 0, (Math.PI/180)*360);
+			c.stroke(); 
+		}
+		if (badGuesses > 3) {
+			drawLine(c, [145,60], [145,130]);
+		}
+		if (badGuesses > 4) {
+			drawLine(c, [145,80], [110,90]);
+		}
+		if (badGuesses > 5) {
+			drawLine(c, [145,80], [180,90]);
+		}
+		if (badGuesses > 6) {
+			drawLine(c, [145,130], [130,170]);
+		}
+		if (badGuesses > 7) {
+			drawLine(c, [145,130], [160,170]);
+			c.fillText('Game over', 45, 110);
+			letters.innerHTML = '';
+			setTimeout(showResult, 200);
+			localStorage.setItem('hangmanLose', 1 + parseInt(localStorage.getItem('hangmanLose')));
+			setTimeout(showScore, 2000);
+		}
+	}
+	if (correctGuesses == wordLength) {
+		letters.innerHTML = '';
+		c.fillText('You won!', 45,110);
+}
+}
+
+
+function drawLine(context, from, to) {
+	context.beginPath();
+	context.moveTo(from[0], from[1]);
+	context.lineTo(to[0], to[1]);
+	context.stroke();
+}
+
+function showResult() {
+	var placeholders = word.innerHTML;
+    placeholders = placeholders.split('');
+	for (i = 0; i < wordLength; i++) {
+		if (placeholders[i] == '_') {
+			placeholders[i] = '<span style="color:red">' + wordToGuess.charAt(i).toUpperCase() + '</span>';
+		}
+	}
+	word.innerHTML = placeholders.join('');
+}
+
 
 function resetScore() {
 	alert('Score has been reset');
